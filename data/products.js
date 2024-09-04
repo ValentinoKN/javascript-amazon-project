70c990f49d422cb89945152df090bc87cc01c498
ValentinoKN <valentino.k.naidoo22@gmail.com>
2024-09-04 08:38:59 +0200

    Learn error handling

--- data/products.js
+++ data/products.js
@@ -103,10 +103,13 @@ export function loadProductsFetch() {
     });
 
     console.log('load products');
+  }).catch((error) => {
+    console.log('Unexpected error. Please try again later.');
   });
 
   return promise;
 };
+
 /*
 loadProductsFetch().then(() => {
   console.log('Next Step');
@@ -129,6 +132,10 @@ export function loadProducts(fun) {
     fun();
   });
 
+  xhr.addEventListener('error', (error) => {
+    console.log('Unexpected error. Please try again later.');
+  });
+ 
   xhr.open('GET','https://supersimplebackend.dev/products');
   xhr.send();
 };
